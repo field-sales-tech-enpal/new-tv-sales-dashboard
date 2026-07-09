7da83555bb26cf5f8ce7cc936dcb63ba6cfe06f8
field-sales-tech-enpal <fieldsalestech@enpal.de>
2026-07-09 15:32:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,18 @@ window.DASHBOARD_CONFIG = {
   ENABLE_AUTO_REFRESH: true,
   ENABLE_TRIGGER_INTERRUPTIONS: true,
 
-  TEAMS_TO_SHOW: ["NOVA", "VF", "MHM"]
+  TEAMS_TO_SHOW: ["NOVA", "VF", "MHM"],
+
+  CELEBRATIONS: {
+    IDV: {
+      title: "🎉 NEW IDV 🎉",
+      gif: "https://media.giphy.com/media/Is1O1TWV0LEJi/giphy.gif",
+      sound: "sounds/idv.mp3"
+    },
+    TBK: {
+      title: "🏆 NEW TBK 🏆",
+      gif: "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif",
+      sound: "sounds/tbk.mp3"
+    }
+  }
 };
